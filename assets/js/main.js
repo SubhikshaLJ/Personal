550e2ff7eb83683b5360ebb0b8f9af59960c1373
Subhiksha LJ <86915212+SubhikshaLJ@users.noreply.github.com>
2021-08-11 22:37:54 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*Sweet alert */
+function alerting(){
+    Swal.fire({
+        title: 'Whatsapp ',
+        text: '93602 30757',
+        imageUrl: 'https://image.flaticon.com/icons/svg/220/220236.svg',
+        imageWidth: 100,
+        imageHeight: 100,
+     
+      })
+}
